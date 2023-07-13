33b728e8fc972646f6ab73136c41035105521e96
Marc <marc.roullea@gmail.com>
2023-07-13 16:15:06 +0200

    Fixes route + return void

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,9 @@
 <?php
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
-
-Router::plugin(
+$routes->plugin(
     'SimpleSaml',
     ['path' => '/simple-saml'],
-    function (RouteBuilder $routes) {
+    function ($routes) {
         $routes->fallbacks(DashedRoute::class);
     }
 );
